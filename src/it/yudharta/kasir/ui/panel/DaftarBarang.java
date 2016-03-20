3adbc5eb6fa7cde05ff81d2d6f7e928705fd87bd
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-20 18:53:38 +0700

    hapus default Table Model

--- src/it/yudharta/kasir/ui/panel/DaftarBarang.java
+++ src/it/yudharta/kasir/ui/panel/DaftarBarang.java
@@ -51,13 +51,10 @@ public class DaftarBarang extends javax.swing.JPanel {
 
         tblDaftar.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+
             }
         ));
         jScrollPane1.setViewportView(tblDaftar);
